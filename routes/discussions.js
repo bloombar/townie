4acf4e10b009a452f67b9001f99746419ef4e373
Amos Bloomberg <amos@cs.nyu.edu>
2017-03-10 17:26:40 -0500

    initial project version

---
+++ routes/discussions.js
@@ -0,0 +1,15 @@
+var express = require('express');
+var router = express.Router();
+
+// middleware that is specific to this router
+router.use(function timeLog (req, res, next) {
+  console.log('Time: ', Date.now())
+  next()
+})
+
+/* GET home page. */
+router.get('/', function(req, res, next) {
+  res.render('discussions', { title: 'Discussions' });
+});
+
+module.exports = router;
